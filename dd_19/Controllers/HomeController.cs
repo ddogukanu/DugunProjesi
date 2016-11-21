bae65bb7fdc7197a247bd8a2040986aed12a4d53
ddogukanu <dogukan.unal@gmail.com>
2016-11-21 13:43:47 +0300

    Add project files.

---
+++ dd_19/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using dd_19.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace dd_19.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Slider()
+        {
+            return View();
+        }
+
+        public ActionResult Story()
+        {
+            return View();
+        }
+
+        public ActionResult Timer()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Wedding()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Rezervasyon()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Rezervasyon(Rezervasyon r)
+        {
+            if(ModelState.IsValid)
+            {
+                r.IP = Request.ServerVariables["REMOTE_ADDR"];
+                ApplicationDbContext ctx = new ApplicationDbContext();
+                ctx.Rezervasyonlar.Add(r);
+                ctx.SaveChanges();
+                ViewBag.sonuc = "TesekkurEt();";
+            }
+
+            return View();
+        }
+
+    }
+}
---
+++ dd_19/Models/Rezervasyon.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dd_19.Models
+{
+    public class Rezervasyon
+    {
+        [Key]
+        public int RezervasyonID { get; set; }
+
+        [Required]
+        [Display(Name ="Ad Soyad")]
+        public string AdSoyad { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        public string Mesaj { get; set; }
+
+        [Display(Name ="Katılacak mısınız?")]
+        public bool KatilacakMi { get; set; }
+        public DateTime Tarih { get; set; }
+        public string IP { get; set; }
+        public Rezervasyon()
+        {
+            Tarih = DateTime.Now;
+        }
+    }
+}
